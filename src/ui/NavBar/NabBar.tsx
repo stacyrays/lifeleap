e68e83a7b19ee38830e83655fca7e6b4d4fb1b8b
Stacy Suits <stacyrays@gmail.com>
2024-05-15 15:02:57 -0700

    add small screen navbar menu dialog

--- src/ui/NavBar/NabBar.tsx
+++ src/ui/NavBar/NabBar.tsx
@@ -1,11 +1,33 @@
+import * as React from 'react';
 import {useState, useEffect} from 'react';
 import { Link, Outlet } from "react-router-dom";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+// import ListItemText from '@mui/material/ListItemText';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import List from '@mui/material/List';
+// import Divider from '@mui/material/Divider';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
 // import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
 import './NavBar.css';
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="left" ref={ref} {...props} />;
+});
 
 // Augment the palette to include an ochre color
 declare module '@mui/material/styles' {
@@ -46,10 +68,20 @@ export const theme = createTheme({
 });
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
   const [width, setWidth] = useState(window.innerWidth);
   const updateDimensions = () => {
     setWidth(window.innerWidth);
     // setHeight(window.innerHeight);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   useEffect(() => {
@@ -72,12 +104,58 @@ const NavBar = () => {
         </>
       );
     } else {
-      // return (<div style={{color: 'red'}}>hamburger</div>);
-      return (<MenuIcon sx={{color: '#0285FF'}} />);
+      return (<MenuIcon sx={{color: '#0285FF'}} onClick={handleClickOpen} />);
     }
   }
 
-  return (<div className="nav-bar">{menu()}<Outlet /></div>);
+  return (
+    <>
+     <div className="nav-bar">{menu()}
+      <Outlet />
+    </div>
+        <Dialog
+          fullScreen
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Transition}
+        >
+          <AppBar sx={{ position: 'relative' }}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                onClick={handleClose}
+                aria-label="close"
+              >
+                <CloseIcon />
+              </IconButton>
+              <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                Menu
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <div style={{margin: '20px'}}>
+            <ThemeProvider theme={theme}>
+              <Stack spacing={2} direction="column">
+                <Link to="/book"><Button color="blue" variant="text" onClick={handleClose}>Book Session</Button></Link>
+                <Link to="/hypnotherapy"><Button color="blue" variant="text" onClick={handleClose}>Hypnotherapy</Button></Link>
+                <Link to="/river"><Button color="blue" variant="text" onClick={handleClose}>Meet the Team</Button></Link>
+                <Link to="/contact"><Button color="blue" variant="text" onClick={handleClose}>Contact Us</Button></Link>
+              </Stack>
+            </ThemeProvider>
+          </div>
+          {/* <List>
+            <ListItemButton>
+              <ListItemText primary="Book Session" onClick={handleClose} />
+            </ListItemButton>
+            <Divider />
+            <ListItemButton>
+              <ListItemText primary="Hypnotherapy"  onClick={handleClose} />
+            </ListItemButton>
+          </List> */}
+        </Dialog>
+      </>
+   );
 }
 
 export default NavBar;
