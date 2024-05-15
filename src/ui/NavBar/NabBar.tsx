2f5f54e49a66df4248a7683413e0bb223cf3adeb
Stacy Suits <stacyrays@gmail.com>
2024-05-15 14:30:44 -0700

    export theme in one place and adjust logo

--- src/ui/NavBar/NabBar.tsx
+++ src/ui/NavBar/NabBar.tsx
@@ -1,8 +1,49 @@
 import {useState, useEffect} from 'react';
 import { Link, Outlet } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 // import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
 import './NavBar.css';
+
+// Augment the palette to include an ochre color
+declare module '@mui/material/styles' {
+  interface Palette {
+    orange: Palette['primary'];
+    blue: Palette['secondary'];
+  }
+
+  interface PaletteOptions {
+    orange?: PaletteOptions['primary'];
+    blue?: PaletteOptions['secondary'];
+  }
+}
+
+// Update the Button's color options to include an ochre option
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    orange: true;
+    blue: true;
+  }
+}
+
+export const theme = createTheme({
+  palette: {
+    orange: {
+      main: '#FF4E02',
+      light: '#FF611D',
+      dark: '#D03F01',
+      contrastText: '#FFFFFF',
+    },
+    blue: {
+      main: '#0285FF',
+      light: '#2B99FF',
+      dark: '#006ED4',
+      contrastText: '#FFFFFF',
+    },
+  },
+});
 
 const NavBar = () => {
   const [width, setWidth] = useState(window.innerWidth);
@@ -20,18 +61,14 @@ const NavBar = () => {
     if (width >= 700){
       return (
         <>
-          <div className="nav-bar-item">
-            <Link to="/book">Book Session</Link>
-          </div>
-          <div className="nav-bar-item">
-            <Link to="/hypnotherapy">Hypnotherapy</Link>
-          </div>
-          <div className="nav-bar-item">
-            <Link to="/river">Meet River</Link>
-          </div>
-          <div className="nav-bar-item">
-            <Link to="/contact">Contact Us</Link>
-          </div>
+          <ThemeProvider theme={theme}>
+            <Stack spacing={2} direction="row">
+              <Link to="/book"><Button color="blue" variant="text">Book Session</Button></Link>
+              <Link to="/hypnotherapy"><Button color="blue" variant="text">Hypnotherapy</Button></Link>
+              <Link to="/river"><Button color="blue" variant="text">Meet the Team</Button></Link>
+              <Link to="/contact"><Button color="blue" variant="text">Contact Us</Button></Link>
+            </Stack>
+          </ThemeProvider>
         </>
       );
     } else {
